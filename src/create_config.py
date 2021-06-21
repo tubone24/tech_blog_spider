c65f9832d8ae0ea0e11c83d21d166c52b2e3314c
tubone <tubo.yyyuuu@gmail.com>
2021-06-21 17:22:26 +0900

    wip

--- src/create_config.py
+++ src/create_config.py
@@ -12,4 +12,4 @@ db = harperdb.HarperDB(
     username=HARPERDB_USERNAME,
     password=HARPERDB_PASSWORD,)
 
-db.csv_file_load(HARPERDB_SCHEMA, "entry_urls", FILEPATH, action="upsert")
+db.csv_data_load(HARPERDB_SCHEMA, "entry_urls", FILEPATH, action="upsert")
