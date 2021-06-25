39badcda4d694bb5939f09c9ec445065e75185b7
tubone <tubo.yyyuuu@gmail.com>
2021-06-25 17:14:04 +0900

    fix install bug

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget https://www.dropbox.com/s/vai7iasou3nlv50/open_file.zip?dl=0 -O open_file.zip
+wget https://www.dropbox.com/s/8l6ufugc7n99l9w/pytermextract-0_01.zip?dl=0 -O open_file.zip
 unzip open_file.zip
 cd open_file
 python setup.py install
