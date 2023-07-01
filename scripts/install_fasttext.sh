bfa96f6d837e04e77cc2a4acf0bf9efcadd7ea29
tubone(Yu Otsubo) <tubo.yyyuuu@gmail.com>
2023-07-01 21:07:25 +0900

    Update install_fasttext.sh

--- scripts/install_fasttext.sh
+++ scripts/install_fasttext.sh
@@ -2,7 +2,5 @@
 
 # model: https://fasttext.cc/docs/en/language-identification.html
 wget https://www.dropbox.com/s/yjgm028s4j3rrr6/lid.176.bin?dl=0 -O lid.176.bin
-git clone https://github.com/facebookresearch/fastText.git
-cd fastText
 pip install pybind11
-pip install .
+pip install git+https://github.com/cfculhane/fastText
