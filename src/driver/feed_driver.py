bc9d531a2733c1b2604fe5c6de7496b29182485f
tubone <tubo.yyyuuu@gmail.com>
2024-12-12 18:23:45 +0900

    numpy

--- src/driver/feed_driver.py
+++ src/driver/feed_driver.py
@@ -20,7 +20,6 @@ class FeedDriverImpl(FeedDriver):
             maxPoolSize=2,  # コネクションプールの最大サイズ
             minPoolSize=1,  # コネクションプールの最小サイズ
             maxIdleTimeMS=6000,  # アイドル接続を1分で切断
-            keepAlive=True,
         )
         self.db = self.client[database]
         self.entry_urls = self.db.entry_urls
