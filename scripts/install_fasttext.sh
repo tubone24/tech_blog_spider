f38b6f10bae45c33e1ecab594196deccb56d6b54
tubone <tubo.yyyuuu@gmail.com>
2024-12-12 19:47:52 +0900

    numpy

--- scripts/install_fasttext.sh
+++ scripts/install_fasttext.sh
@@ -3,4 +3,4 @@
 # model: https://fasttext.cc/docs/en/language-identification.html
 wget https://www.dropbox.com/s/yjgm028s4j3rrr6/lid.176.bin?dl=0 -O lid.176.bin
 pip install pybind11
-pip install git+https://github.com/cfculhane/fastText
+pip install fasttext-wheel
